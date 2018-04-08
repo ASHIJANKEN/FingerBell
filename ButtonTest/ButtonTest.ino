18761b068a0904d6e840826676c27ee31b426ae0
ASHIJANKEN <24617794+ASHIJANKEN@users.noreply.github.com>
2018-04-09 02:55:16 +0900

    update ino files/ add guide_for_soldering.ai

--- ButtonTest/ButtonTest.ino
+++ ButtonTest/ButtonTest.ino
@@ -1,4 +1,13 @@
-const int BTN_PIN = 4;
+const int BTN_PIN = 12;
+//Button state
+const int NOT_PRESSED         = 0;
+const int PRESSED             = 1;
+const int PRESSING            = 2;
+const int RELEASED            = 3;
+const int LONG_PRESSED        = 4;
+const int LONG_PRESSING       = 5;
+const int LONG_PRESS_RELEASED = 6;
+
 void setup(){
   Serial.begin(38400);
   pinMode(BTN_PIN, INPUT_PULLUP);
@@ -6,45 +15,81 @@ void setup(){
 bool transit = false;
 
 void loop(){
-  a();
+  switch (detectBtnStat()) {
+      case 0:
+        Serial.println("NOT_PRESSED");
+        break;
+      case 1:
+        Serial.println("PRESSED");
+        break;
+      case 2:
+        Serial.println("PRESSING");
+        break;
+      case 3:
+        Serial.println("RELEASED");
+        break;
+      case 4:
+        Serial.println("LONG_PRESSED");
+        break;
+      case 5:
+        Serial.println("LONG_PRESSING");
+        break;
+      case 6:
+        Serial.println("LONG_PRESS_RELEASED");
+        break;
+  }
+  delay(100);
 }
 
-void a(){
-  int old_btn_state = digitalRead(BTN_PIN); // The button's previous state.
-  unsigned long pressed_time = 0;
+int detectBtnStat(){
+  static bool is_long = false; // Manage the button long pressing
+  static int old_btn_val = digitalRead(BTN_PIN); // The button's previous state.
+  static unsigned long pressed_time = 0;           // Time when the button pressed.
+  int button_state = NOT_PRESSED;
 
-  while(true){
-    int btn_state = digitalRead(BTN_PIN);
-    // Serial.print(old_btn_state);
-    // Serial.print(" : ");
-    // Serial.println(btn_state);
+  int btn_val = digitalRead(BTN_PIN);
 
-    // Check the button state.
-    if(!btn_state & (btn_state ^ old_btn_state) & !transit){
-      // When the button is pressed.
-      pressed_time = millis();
-      Serial.println("Pressed!");
+  if(!btn_val & (btn_val ^ old_btn_val) & !is_long){
+    // When the button is pressed.
+    pressed_time = millis();
+    button_state = PRESSED;
 
-    }else if(!btn_state & !old_btn_state & !transit){
+  }else if(!btn_val & !old_btn_val){
+    // When the button is kept pressing.
+
+    if(is_long){
       // When the button is kept pressing over 2 seconds.
-      // if(millis() - pressed_time > 2000){
-      //   Serial.println("Kept pressing!");
-      //   transit = true;
-
-      //   break;
-      // }
-
-    }else if(btn_state & (btn_state ^ old_btn_state)){
-      // When the button is released.
-      if(millis() - pressed_time > 100){
-        Serial.println("Released!");
-        transit = false;
-        delay(80);
-        break;
+      button_state = LONG_PRESSING;
+    }else{
+      if(millis() - pressed_time > 2000){
+        button_state = LONG_PRESSED;
+        is_long = true;
+      }else{
+        button_state = PRESSING;
       }
     }
 
-    old_btn_state = btn_state;
+  }else if(btn_val & (btn_val ^ old_btn_val)){
+    // When the button is released.
+
+    if(is_long){
+      //When the button is released after long pressing.
+      button_state = LONG_PRESS_RELEASED;
+      is_long = false;
+      delay(80);
+
+    }else{
+      if(millis() - pressed_time > 10){
+        button_state = RELEASED;
+        delay(80);
+      }
+    }
+
+  }else{
+    button_state = NOT_PRESSED;
   }
-  Serial.println("-----------------");
+
+  old_btn_val = btn_val;
+
+  return button_state;
 }
--- MainControl/MainControl.ino
+++ MainControl/MainControl.ino
@@ -3,9 +3,9 @@
 #include <Servo.h>
 
 // Pin assign of rotary encoders(A/B phases)
-const int ENCODERS_PINS[][] = {{0, 1},
-                                {2, 3},
-                                {4, 5}
+const int ENCODERS_PINS[3][2] = {{2, 3},
+                                {4, 5},
+                                {6, 7}
                               };
 
 // Pin assign of a button.
@@ -15,52 +15,58 @@ const int BTN_PIN = 12;
 const int SERVO_PIN = 11;
 
 // Pin assign of a lcd.
-const int RS       = 6;
-const int ENABLE   = 7;
+const int RS       = 14;
+const int ENABLE   = 15;
 const int DB4      = 8;
 const int DB5      = 9;
 const int DB6      = 10;
 const int DB7      = 13;
 
 // For servo
-const int MIN_ANGLE = 70;
+const int MIN_ANGLE = 90;
 const int MAX_ANGLE = 180;
 
 // Modes
 const int SET_TIMER   = 0;
 const int START_TIMER = 1;
 
+//Button state
+const int NOT_PRESSED         = 0;
+const int PRESSED             = 1;
+const int PRESSING            = 2;
+const int RELEASED            = 3;
+const int LONG_PRESSED        = 4;
+const int LONG_PRESSING       = 5;
+const int LONG_PRESS_RELEASED = 6;
+
 // Waiting time to avoid chattering.
 const int WAIT_FOR_BOUNCE_MSEC = 100;
 
 // Addresses of EEPROM for keeping ringing time.
-const int RING_SECONDS_ADDR0 = 0;
-const int RING_SECONDS_ADDR1 = 1;
-const int RING_SECONDS_ADDR2 = 2;
+const int RING_ADDR_OFFSET = 500;
 
 // Time when the bell rings.
 int ring_seconds[3] = {0, 0, 0};
 
-// Manage the button long pressing
-bool transit = false;
-
 // Present encoders state
-int enc_state[][] = {{0, 0},
+int enc_state[3][2] = {{0, 0},
                       {0, 0},
                       {0, 0}
                     };
 
 // Previous encoders state
-int enc_prev_state[][] = {{0, 0},
+int enc_prev_state[3][2] = {{0, 0},
                             {0, 0},
                             {0, 0}
                           };
 
-// LCD
+// Create objects.
 LiquidCrystal lcd(RS, ENABLE, DB4, DB5, DB6, DB7);
+Servo myservo;
 
 void setup(){
-  // Initialize the LCD.
+  Serial.begin(9600);
+  // Display initializing message.
   lcd.begin(16, 2); //16 chars × 2 rows
   lcd.clear();
   lcd.setCursor(0, 0);
@@ -68,13 +74,19 @@ void setup(){
   lcd.setCursor(0, 1);
   lcd.print("RINGING MACHINE");
 
-  // Initialize a serial.
-  //Serial.begin(4800);
+  //Initialize the servo.
+  myservo.attach(SERVO_PIN);
 
   // Get ringing time from EEPROM.
-  // ring_seconds[0] = (int)EEPROM.read(RING_SECONDS_ADDR0);
-  // ring_seconds[1] = (int)EEPROM.read(RING_SECONDS_ADDR1);
-  // ring_seconds[2] = (int)EEPROM.read(RING_SECONDS_ADDR2);
+  Serial.println("-----------------");
+  for(int i = 0; i < 3; i++){
+    Serial.println(RING_ADDR_OFFSET + i*2);
+    int upper = EEPROM.read(RING_ADDR_OFFSET + i*2);
+    Serial.println(EEPROM.read(RING_ADDR_OFFSET + i*2));
+    int lower = EEPROM.read(RING_ADDR_OFFSET + i*2 + 1);
+    Serial.println(EEPROM.read(RING_ADDR_OFFSET + i*2 + 1));
+    ring_seconds[i] = (int)(upper << 8 | lower);
+  }
 
   // Disable an internal LED.
   pinMode(13,OUTPUT);
@@ -93,123 +105,114 @@ void setup(){
 
   // Initialize a servo.
   pinMode(SERVO_PIN,OUTPUT);
+
+  lcd.clear();
 }
 
 void loop(){
 
   // set timer to ring the bell.
   setTimer();
+  lcd.clear();
 
   // start timer.
   startTimer();
+  lcd.clear();
 
 }
 
 // set time to ring the bell.
 void setTimer(){
-  int old_btn_state = digitalRead(BTN_PIN); // The button's previous state.
-  unsigned long pressed_time = 0;           // Time when the button pressed.
-
+  Serial.print(ring_seconds[0]);
+  Serial.print(" : ");
+  Serial.print(ring_seconds[1]);
+  Serial.print(" : ");
+  Serial.print(ring_seconds[2]);
+  Serial.println(" : ");
   while(true){
-    unsigned long pressed_time = 0;
-    int btn_state = digitalRead(BTN_PIN);
-
     // Display setting information on LCD.
     displayInfo(SET_TIMER, ring_seconds);
 
     // Adjust ringing time with rotary encoder and wait a short period to avoid chattering.
     for(int i=0; i<3; i++){
-      int a = 2 * i; // Port number for A phase
+      int a = 2 * (i + 1); // Port number for A phase
       int b = a + 1; // Port number for B phase
       enc_state[i][0] = (PIND & _BV(a)) >> a;
       enc_state[i][1] = (PIND & _BV(b)) >> b;
 
       if(enc_state[i][0] != enc_prev_state[i][0] || enc_state[i][1] != enc_prev_state[i][1] ){
-        ring_seconds[i] += readEnc(enc_prev_state[i][0], enc_prev_tate[i][1], enc_state[i][0], enc_state[i][1]) * 10;
+        ring_seconds[i] += readEnc(enc_prev_state[i][0], enc_prev_state[i][1], enc_state[i][0], enc_state[i][1]) * 10;
+        if(ring_seconds[i] < 0) ring_seconds[i] = 0;
       }
 
       enc_prev_state[i][0] = enc_state[i][0];
       enc_prev_state[i][1] = enc_state[i][1];
     }
-    // delay(WAIT_FOR_BOUNCE_MSEC);
 
     // Check the button state.
-    if(!btn_state & (btn_state ^ old_btn_state) & !transit){
-      // When the button is pressed.
-      pressed_time = millis();
-
-    }else if(!btn_state & !old_btn_state & !transit){
-      // When the button is kept pressing over 2 seconds.
-      // Goto preset mode.
-      // TODO: Implement preset mode.
-      // break;
-
-    }else if(btn_state & (btn_state ^ old_btn_state)){
-      // When the button is released.
-      // Finish setting.
-      if(millis() - pressed_time > WAIT_FOR_BOUNCE_MSEC){
-        transit = false;
-        break;
-      }
+    switch (detectBtnStat()){
+        case RELEASED:
+          //save ring_seconds to EEPROM.
+          for(int i = 0; i < 3; i++){
+            EEPROM.write(RING_ADDR_OFFSET + i*2, ring_seconds[i] >> 8);
+            EEPROM.write(RING_ADDR_OFFSET + i*2 + 1, ring_seconds[i] & 0xFF);
+            Serial.println(RING_ADDR_OFFSET + i*2);
+            // EEPROM.write(RING_ADDR_OFFSET + i*2, i*10 >> 8);
+            Serial.println(ring_seconds[i] >> 8);
+            // EEPROM.write(RING_ADDR_OFFSET + i*2 + 1, i*10 & 0xFF);
+            Serial.println(ring_seconds[i] & 0xFF);
+          }
+          return;
+          break;
+        // case LONG_PRESSED:
+          // TODO: Implement preset mode.
+          // break;
     }
-
-    old_btn_state = btn_state;
   }
 }
 
 void startTimer(){
-  int old_btn_state = digitalRead(BTN_PIN); // The button's previous state.
-  unsigned long pressed_time = 0;           // Time when the button pressed.
   int is_stopping = 0;                      // Is the timer stops?
   unsigned long start_time = millis();      // Time when this method starts[msec].
   unsigned long suspension_start_time = 0;  // Time when stops timer[msec].
-  unsigned long total_suspension_time = 0;  // Total elapsed time when timer stops[msec].
-                                            // int elapsed_time = 0;                       // Elpsed time[sec].
-  int ranged_bell[3] = [0, 0, 0];           // See whether it already rang the bell or not
-  int args[2] = [0, 0];                     // [is_stopping, Elapsed time]
+  unsigned long total_suspension_time = 0;  // Total elapsed time while timer stops[msec].
+  int rang_bell[3] = {0, 0, 0};           // See whether it already rang the bell or not
+  int args[2] = {0, 0};                     // [is_stopping, Elapsed time]
 
   // Start timer.
   // Control the servo when it's ringing time.
   while(true){
-    int btn_state = digitalRead(BTN_PIN);
-
     // Display information on LCD.
     displayInfo(START_TIMER, args);
 
     // If it is ringing time, ring the bell.
-    if(args[1] == ring_seconds[0] && ranged_bell[0] == 0){
+    if(args[1] == ring_seconds[0] && !rang_bell[0]){
       ringBell(1);
-      ranged_bell[0] = !ranged_bell[0];
-    }else if(args[1] == ring_seconds[1] && ranged_bell[1] == 0){
+      rang_bell[0] = !rang_bell[0];
+    }else if(args[1] == ring_seconds[1] && !rang_bell[1]){
       ringBell(2);
-      ranged_bell[1] = !ranged_bell[1];
-    }else if(args[1] == ring_seconds[2] && ranged_bell[2] == 0){
+      rang_bell[1] = !rang_bell[1];
+    }else if(args[1] == ring_seconds[2] && !rang_bell[2]){
       ringBell(3);
-      ranged_bell[2] = !ranged_bell[2];
+      rang_bell[2] = !rang_bell[2];
     }
 
-    // Update the elapsed time.
-    args[1] = (int)((millis() - start_time - total_suspension_time) / 1000L);
+    // Update args.
+    //Update is_stopping.
+    args[0] = is_stopping;
+    //Update elapsed time.
+    if(!is_stopping){
+      args[1] = (int)((millis() - start_time - total_suspension_time) / 1000L);
+    }
 
     // Check the button state.
-    if(!btn_state & (btn_state ^ old_btn_state) & !transit){
-      // When the button is pressed.
-      pressed_time = millis();
-
-    }else if(!btn_state & !old_btn_state & !transit){
-      // When the button is kept pressing over 2 seconds.
-      // Return to setTimer.
-      if(millis() - pressed_time > 2000){
-        transit = true;
+    switch(detectBtnStat()){
+      case LONG_PRESSING:
+        return;
         break;
-      }
-
-    }else if(btn_state & (btn_state ^ old_btn_state)){
-      // When the button is released.
-      // Stop/restart timer.
-      if(millis() - pressed_time > WAIT_FOR_BOUNCE_MSEC){
+      case PRESSED:
+        // Stop/restart timer.
         is_stopping = !is_stopping;
-        transit = false;
 
         if(is_stopping != 0){
           // When the timer stops.
@@ -218,10 +221,8 @@ void startTimer(){
           //When the timer restarts.
           total_suspension_time += millis() - suspension_start_time;
         }
-      }
+        break;
     }
-
-    old_btn_state = btn_state;
   }
 }
 
@@ -229,21 +230,17 @@ void startTimer(){
 void displayInfo(int mode, int* args){
   switch (mode) {
     case SET_TIMER:
-      //上の段
       lcd.setCursor(0, 0);
-      lcd.print("   1     2     3");
-
-      //下の段
-      lcd.setCursor(0,1);
       // 1st ringing time
+      lcd.print("1:");
       printTime(*args);
-      lcd.print(" ");
       // 2nd ringing time
+      lcd.print(" 2:");
       printTime(*(args+1));
-      lcd.print(" ");
+      lcd.setCursor(0, 1);
       // 3rd ringing time
+      lcd.print("3:");
       printTime(*(args+2));
-      lcd.print(" ");
       break;
 
     case START_TIMER:
@@ -251,18 +248,16 @@ void displayInfo(int mode, int* args){
       lcd.setCursor(0, 0);
       if(*args == 0){
         // When measuring
-        lcd.print("Measuring...")
+        lcd.print("Running...");
       }else{
         // When measuring is suspended
-        lcd.print("Stop!")
+        lcd.print("Stop!     ");
       }
 
       //下の段
-      lcd.setCursor(0,1);
-      lcd.print("     ");
+      lcd.setCursor(5,1);
       // now
       printTime(*(args+1));
-      lcd.print("      ");
       break;
 
     default:
@@ -275,11 +270,13 @@ int readEnc(uint8_t prev_a, uint8_t prev_b, uint8_t a, uint8_t b){
   uint8_t encoded  = (prev_a << 4) | (prev_b << 3) | (a << 2) | b;
 
   // if(encoded == 0b1101 || encoded == 0b0100 || encoded == 0b0010 || encoded == 0b1011){
-  if(encoded == 0b1101){
+  if(encoded == 0b0100|| encoded == 0b0010){
+    // delay(WAIT_FOR_BOUNCE_MSEC);
     return 1;
-  // } else if(encoded == 0b1110 || encoded == 0b0111 || encoded == 0b0001 || encoded == 0b1000) {
-  }else if(encoded == 0b1110) {
+  // } else if(encoded == 0b0010 || encoded == 0b0111 || encoded == 0b0001 || encoded == 0b1000) {
+  }else if(encoded == 0b0001) {
     return -1;
+    // delay(WAIT_FOR_BOUNCE_MSEC);
   }else{
     return 0;
   }
@@ -301,11 +298,11 @@ void printTime(int total_seconds){
 // Control the servo to ring the bell.
 // This method is supposed to be completed in one second.
 void ringBell(int times){
-  int stroke_time = 1000 / times;                                   // Time for one stroke[msec].
-  int delay_msec = stroke_time / ((MAX_ANGLE - MIN_ANGLE) * 2 + 1); // Calculate delay to finish all strokes in one second.
+  float stroke_time = 333;             // Time for one stroke[msec].
+  int delay_msec = (int)(stroke_time / ((MAX_ANGLE - MIN_ANGLE) * 2 + 1)); // Calculate delay to finish all strokes in one second.
 
   // Push an arm.
-  for(int i=0; i<3; i++){
+  for(int i=0; i<times; i++){
     for(int pos = MIN_ANGLE; pos <= MAX_ANGLE; pos++){
       myservo.write(pos);
       delay(delay_msec);
@@ -317,4 +314,57 @@ void ringBell(int times){
       delay(delay_msec);
     }
   }
+}
+
+int detectBtnStat(){
+  static bool is_long = false; // Manage the button long pressing
+  static int old_btn_val = digitalRead(BTN_PIN); // The button's previous state.
+  static unsigned long pressed_time = 0;           // Time when the button pressed.
+  int button_state = NOT_PRESSED;
+
+  int btn_val = digitalRead(BTN_PIN);
+
+  if(!btn_val & (btn_val ^ old_btn_val) & !is_long){
+    // When the button is pressed.
+    pressed_time = millis();
+    button_state = PRESSED;
+
+  }else if(!btn_val & !old_btn_val){
+    // When the button is kept pressing.
+
+    if(is_long){
+      // When the button is kept pressing over 2 seconds.
+      button_state = LONG_PRESSING;
+    }else{
+      if(millis() - pressed_time > 2000){
+        button_state = LONG_PRESSED;
+        is_long = true;
+      }else{
+        button_state = PRESSING;
+      }
+    }
+
+  }else if(btn_val & (btn_val ^ old_btn_val)){
+    // When the button is released.
+
+    if(is_long){
+      //When the button is released after long pressing.
+      button_state = LONG_PRESS_RELEASED;
+      is_long = false;
+      delay(80);
+
+    }else{
+      if(millis() - pressed_time > 10){
+        button_state = RELEASED;
+        delay(80);
+      }
+    }
+
+  }else{
+    button_state = NOT_PRESSED;
+  }
+
+  old_btn_val = btn_val;
+
+  return button_state;
 }
